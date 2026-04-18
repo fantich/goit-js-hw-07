f075ea69a3ede782edf66afb1f76e91ff1dbbf99
fantich <a.fantich@gmail.com>
2026-04-18 13:32:00 +0200

    task-1_fix

--- js/task-1.js
+++ js/task-1.js
@@ -1,12 +1,11 @@
 const categoriesList = document.querySelector('#categories');
-
 const categoryItems = categoriesList.querySelectorAll('.item');
 
 console.log(`Number of categories: ${categoryItems.length}`);
 
 categoryItems.forEach(item => {
   const categoryName = item.querySelector('h2').textContent;
-    const elementsCount = item.querySelectorAll('ul li').length;
+  const elementsCount = item.querySelectorAll('ul li').length;
 
   console.log(`Category: ${categoryName}`);
   console.log(`Elements: ${elementsCount}`);
